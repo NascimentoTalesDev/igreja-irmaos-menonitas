01ad6c14c4f66540278a944e7131e3bada4303fc
NascimentoTalesDev <111396464+NascimentoTalesDev@users.noreply.github.com>
2024-02-26 11:25:44 -0300

    doc: add colors taiwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,17 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors:{
+      primary:"#FF6B05",
+      primary_more:"#D95800",
+      primary_less:"#FF7A1F",
+      secondary:"#171C22",
+      secondary_more:"#0C1014",
+      secondary_less:"#2E343B",
+      success:"#1CD174",
+      danger:"#FF5658",
+      light:"#FFFFFF"
+    }
   },
   plugins: [],
 };

27742cc2cd856ac7851b237e1e7c836d02f3385a
NascimentoTalesDev <111396464+NascimentoTalesDev@users.noreply.github.com>
2024-02-26 11:18:55 -0300

    doc: initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+
+const font = Inter({ subsets: ["latin"] });
+export default function App({ Component, pageProps }: AppProps) {
+  return(
+    <div className={`${font.className}`}>
+      <Component {...pageProps} />
+    </div>
+  ) 
+}

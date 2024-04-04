ea50c1987b3352a9778a335960bf758c886f3906
NascimentoTalesDev <111396464+NascimentoTalesDev@users.noreply.github.com>
2024-04-04 16:36:41 -0300

    feat: add report useefect

--- pages/api/v1/upload-files.js
+++ pages/api/v1/upload-files.js
@@ -3,7 +3,7 @@ import fs from "fs"
 import { PutObjectCommand, S3Client, DeleteObjectCommand, GetObjectAclCommand } from "@aws-sdk/client-s3";
 import mime from "mime-types"
 
-const bucketName = "igreja-irmaos-menonitas"
+const bucketName = "igreja-irmaos-menonitas-2"
 
 export default async function handle(req, res) {
     const { method } = req

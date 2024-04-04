1c9e0d5557cf655979a81ca6f0afa34d2cd311c5
NascimentoTalesDev <111396464+NascimentoTalesDev@users.noreply.github.com>
2024-04-04 16:52:27 -0300

    feat: add mongodb and awss3

--- pages/api/v1/documents/id/[id].js
+++ pages/api/v1/documents/id/[id].js
@@ -3,7 +3,7 @@ import { Document } from "@/models/Document";
 import { Log } from "@/models/Log";
 import bcrypt from "bcrypt"
 import { S3Client, DeleteObjectCommand } from "@aws-sdk/client-s3";
-const bucketName = "igreja-irmaos-menonitas"
+const bucketName = "igreja-irmaos-menonitas-2"
 
 export default async function DocumentId(req, res, next) {
     await mongooseConnect()

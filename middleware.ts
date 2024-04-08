a55f6456c075b8d2dc6845a9d2f054897a6cff3b
NascimentoTalesDev <111396464+NascimentoTalesDev@users.noreply.github.com>
2024-04-08 20:19:45 -0300

    fix: change api

--- middleware.ts
+++ middleware.ts
@@ -5,17 +5,7 @@ type RoutePermissions = {
 }
 
 export default function middleware(request: NextRequest, response: NextResponse) {
-  const res = NextResponse.next()
 
-  // add the CORS headers to the response
-  res.headers.append('Access-Control-Allow-Credentials', "true")
-  res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
-  res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
-  res.headers.append(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  
   var token: string | undefined = request.cookies.get("user")?.value
   let rule: string;
 
